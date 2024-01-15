96496232b8aa94b0ea7ee119b057920b9c3030db
Poxvnn <103416451+Poxvnn@users.noreply.github.com>
2024-01-15 16:03:35 +0700

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-mongoose.connect('mongodb+srv://quyettamdqt:poxdeptraivcl@poxdeptraivcl.d9lqd2z.mongodb.net/?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://quyettamdqt:poxdeptraivcl@poxdeptraivcl.d9lqd2z.mongodb.net/main', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
@@ -15,21 +15,35 @@ const creditsSchema = new mongoose.Schema({
   credits: Number
 });
 
-const Credit = mongoose.model('Credit', creditsSchema);
+// Đặt tên collection là "credits"
+const Credit = mongoose.model('Credit', creditsSchema, 'credits');
 
 app.use(bodyParser.json());
+
+// Endpoint để lấy danh sách user_id
+app.get('/main/credits/user_ids', async (req, res) => {
+  try {
+    const userIDs = await Credit.find({}, 'user_id');
+    res.status(200).json(userIDs.map(doc => doc.user_id));
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
+});
 
 app.post('/paypal/success', async (req, res) => {
   try {
     const user_id = req.query.user_id;
     const creditsAdded = req.body.quantity;
 
+    // Cập nhật số credits trong MongoDB
     const updatedCredits = await Credit.findOneAndUpdate(
       { user_id: user_id },
       { $inc: { credits: creditsAdded } },
       { new: true, upsert: true }
     );
 
+    // Gửi lại URL đã được cập nhật với số credits mới
     const updatedURL = `https://api.telegram.org/bot6560826400:AAHRmt4LcQRrd39cB8ebYmUrOxGDVK3PBos/sendMessage?text=done&chat_id=${user_id}`;
     res.status(200).json({ updatedURL, updatedCredits });
   } catch (error) {
