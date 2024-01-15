9fe5a6c27b922b322140803120bb7fbeddcbeb53
Poxvnn <103416451+Poxvnn@users.noreply.github.com>
2024-01-15 15:49:30 +0700

    Update server.js

--- server.js
+++ server.js
@@ -5,13 +5,11 @@ const mongoose = require('mongoose');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Kết nối MongoDB
 mongoose.connect('mongodb+srv://quyettamdqt:poxdeptraivcl@poxdeptraivcl.d9lqd2z.mongodb.net/?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
 
-// Định nghĩa schema và model cho dữ liệu credits
 const creditsSchema = new mongoose.Schema({
   user_id: String,
   credits: Number
@@ -19,26 +17,19 @@ const creditsSchema = new mongoose.Schema({
 
 const Credit = mongoose.model('Credit', creditsSchema);
 
-// Middleware để parse JSON từ request body
 app.use(bodyParser.json());
 
-// Route để xử lý thanh toán thành công từ PayPal
 app.post('/paypal/success', async (req, res) => {
   try {
-    // Lấy user_id từ query parameter trong URL
     const user_id = req.query.user_id;
-
-    // Lấy số credits từ request body của PayPal
     const creditsAdded = req.body.quantity;
 
-    // Cập nhật số credits trong MongoDB
     const updatedCredits = await Credit.findOneAndUpdate(
       { user_id: user_id },
       { $inc: { credits: creditsAdded } },
       { new: true, upsert: true }
     );
 
-    // Gửi lại URL đã được cập nhật với số credits mới
     const updatedURL = `https://donepoxvnn.github.io?user_id=${user_id}`;
     res.status(200).json({ updatedURL, updatedCredits });
   } catch (error) {
